fbdf5ceb50579bad2b18e47268d5a87e8cbf0ce1
Michael Hutchinson <m.j.hutchinson@gmail.com>
2013-07-06 13:45:54 -0400

    Render Quad

--- DogGame.cs
+++ DogGame.cs
@@ -1,7 +1,8 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
-using Cocos2D;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace WhatsInTheMountain
 {
@@ -11,38 +12,22 @@ namespace WhatsInTheMountain
 
 		public DogGame ()
 		{
-			graphics = new GraphicsDeviceManager (this);
+			const int preferredWidth = 1024;
+			const int preferredHeight = 768;
+
 			Content.RootDirectory = "Content";
+
+			graphics = new GraphicsDeviceManager (this) {
+				PreferredBackBufferWidth = preferredWidth,
+				PreferredBackBufferHeight = preferredHeight
+			};
 			graphics.IsFullScreen = false;
 
-			// Frame rate is 30 fps by default for Windows Phone.
-			TargetElapsedTime = TimeSpan.FromTicks (333333 / 2);
-
-			// Extend battery life under lock.
-			//InactiveSleepTime = TimeSpan.FromSeconds(1);
-
-			var application = new DogApplication (this, graphics);
-			Components.Add (application);
-		}
-
-		private void ProcessBackClick ()
-		{
-			if (CCDirector.SharedDirector.CanPopScene) {
-				CCDirector.SharedDirector.PopScene ();
-			} else {
-				Exit ();
-			}
+			Components.Add (new Tunnel (this));
 		}
 
 		protected override void Update (GameTime gameTime)
 		{
-			// Allows the game to exit
-			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
-				ProcessBackClick ();
-			}
-
-			// TODO: Add your update logic here
-
 			base.Update (gameTime);
 		}
 	}
--- Tunnel.cs
+++ Tunnel.cs
@@ -1,28 +1,121 @@
 using System;
 using System.Runtime.InteropServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
 
 namespace WhatsInTheMountain
 {
-	public class Tunnel
+	public class Tunnel : DrawableGameComponent
 	{
-		//float playerPosition, dogPosition;
+		BasicEffect basicEffect;
+		List<Texture2D> wallTextures = new List<Texture2D> ();
+		TunnelLayer [] layers = new TunnelLayer [32];
+		Matrix view, projection;
+		Vector3 cameraPosition;
+		short[] clockwiseQuadIndexes = { 0, 1, 2, 2, 1, 3 };
 
-		TunnelSegment[] segments = new TunnelSegment[128];
-		int lastGenerated;
-
-		void GenerateNextSegment ()
+		public Tunnel (Game game) : base (game)
 		{
-			var seg = new TunnelSegment ();
+		}
 
-			segments [lastGenerated] = seg;
-			lastGenerated = (lastGenerated + 1 + 128) % 128;
+		public override void Initialize ()
+		{
+			cameraPosition = new Vector3 (0, 0, 2);
+			view = Matrix.CreateLookAt (cameraPosition, Vector3.Zero, Vector3.Up);
+			projection = Matrix.CreatePerspectiveFieldOfView (
+				MathHelper.ToRadians (70),
+				GraphicsDevice.DisplayMode.AspectRatio,
+				1,
+				500);
+
+			basicEffect = new BasicEffect (GraphicsDevice) {
+				World = Matrix.Identity,
+				View = view,
+				Projection = projection,
+				TextureEnabled = true
+			};
+			basicEffect.EnableDefaultLighting ();
+
+			base.Initialize ();
+		}
+
+		protected override void LoadContent ()
+		{
+			const int earthTextures = 2;
+			for (int i = 0; i < 2; i++) {
+				wallTextures.Add (Game.Content.Load<Texture2D> ("earth" + i.ToString ()));
+			}
+
+			base.LoadContent ();
+		}
+
+		public override void Update (GameTime gameTime)
+		{
+			base.Update (gameTime);
+		}
+
+		public override void Draw (GameTime gameTime)
+		{
+			GraphicsDevice.Clear (Color.CornflowerBlue);
+			var vertices = new VertexPositionNormalTexture[4];
+
+			foreach (var layer in layers) {
+				//foreach (var l in layer.
+				FillQuadVertices (vertices, Vector3.Zero, Vector3.Backward, Vector3.Up, 1, 1);
+				basicEffect.Texture = wallTextures [1];
+
+				foreach (EffectPass pass in basicEffect.CurrentTechnique.Passes) {
+					pass.Apply ();
+
+					GraphicsDevice.DrawUserIndexedPrimitives <VertexPositionNormalTexture> (
+						PrimitiveType.TriangleList,
+						vertices, 0, 4,
+						clockwiseQuadIndexes, 0, 2);
+				}
+			}
+
+			base.Draw (gameTime);
+		}
+
+		void FillQuadVertices (VertexPositionNormalTexture[] vertices, Vector3 origin, Vector3 normal, Vector3 up, float width, float height)
+		{
+			// Calculate the quad corners
+			var left = Vector3.Cross (normal, up);
+			Vector3 uppercenter = (up * height / 2) + origin;
+			var upperLeft = uppercenter + (left * width / 2);
+			var upperRight = uppercenter - (left * width / 2);
+			var lowerLeft = upperLeft - (up * height);
+			var lowerRight = upperRight - (up * height);
+
+			// Fill in texture coordinates to display full texture
+			// on quad
+			Vector2 textureUpperLeft = new Vector2( 0.0f, 0.0f );
+			Vector2 textureUpperRight = new Vector2( 1.0f, 0.0f );
+			Vector2 textureLowerLeft = new Vector2( 0.0f, 1.0f );
+			Vector2 textureLowerRight = new Vector2( 1.0f, 1.0f );
+
+			// Provide a normal for each vertex
+			for (int i = 0; i < vertices.Length; i++) {
+				vertices[i].Normal = normal;
+			}
+
+			// Set the position and texture coordinate for each
+			// vertex
+			vertices[0].Position = lowerLeft;
+			vertices[0].TextureCoordinate = textureLowerLeft;
+			vertices[1].Position = upperLeft;
+			vertices[1].TextureCoordinate = textureUpperLeft;
+			vertices[2].Position = lowerRight;
+			vertices[2].TextureCoordinate = textureLowerRight;
+			vertices[3].Position = upperRight;
+			vertices[3].TextureCoordinate = textureUpperRight;
 		}
 	}
 
-	public unsafe struct TunnelSegment
+	public unsafe struct TunnelLayer
 	{
-		public float XOffset, YOffset;
-		public fixed int textureID [8];
-		public fixed int obstacleID [8]; 
+		public fixed int TextureID [8];
+		public fixed int ObstacleID [8];
 	}
 }
